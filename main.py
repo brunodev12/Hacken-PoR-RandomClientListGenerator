1324d3efe735b1f0ccf88e2b0e3ab45ae89dc677
brunodev12 <bruno.carthery12@gmail.com>
2023-11-01 13:49:17 -0300

    feat: added '--token' argument

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils.client_generator import generate_client_list
 from utils.file_ops import save_json
 from utils.arguments import parse_arguments
+from utils.check_arguments import check_tokens
 
 def run():
     args = parse_arguments()
@@ -12,8 +13,16 @@ def run():
     if not args.file.endswith('.json'):
         print("Error: The output file name must end with '.json'.")
         return
+    
+    if args.tokens:
+        tokens = check_tokens(args.tokens)
+        if tokens==[]:
+            print("Error: Token not found")
+            return
+    else:
+        tokens = []
 
-    client_list = generate_client_list(args.users)
+    client_list = generate_client_list(args.users, tokens=tokens)
 
     save_json(client_list, args.file)
 
--- utils/client_generator.py
+++ utils/client_generator.py
@@ -5,30 +5,47 @@ from .file_ops import load_json
 
 currencies_list = load_json("currencies_list.json")
 
-def generate_client_list(length):
+def generate_client_list(length, tokens=[]):
     client_list = []
 
-    for i in range(length):
+    for _ in range(length):
         _uuid = uuid.uuid4()
         client_id = str(_uuid)  # User IDs start from '1'
         # Generate a random number between 1 and 10
-        num_tokens_accounts = random.randint(1, 10)
+
+        if tokens:
+            num_tokens_accounts = len(tokens)
+        else:
+            num_tokens_accounts = random.randint(1, 10)
 
         accounts = []
         random_token_list = []
 
         for j in range(num_tokens_accounts):
-            random_token = random.randint(0, len(currencies_list)-1)
-            if random_token not in random_token_list:
-                token_list = currencies_list[random_token]
-                token_name = token_list[0]
+            if tokens:
+                token_name = tokens[j]
+                for token_list in currencies_list:
+                    if token_name == token_list[0]:
+                        minimun = token_list[1]
+                        maximun = token_list[2]
+                        account_tokens = generate_number(
+                            minimun, maximun, currency=token_name)
+                        accounts.append((token_name, account_tokens))
+                        break
+            else:
+                random_token = random.randint(0, len(currencies_list)-1)
+                if random_token not in random_token_list:
+                    token_list = currencies_list[random_token]
+                    token_name = token_list[0]
+                    minimun = token_list[1]
+                    maximun = token_list[2]
 
-                # Generate a random number between 1e-9 and 1e9
-                account_tokens = generate_number(
-                    token_list[1], token_list[2], currency=token_name)
+                    # Generate a random number between 1e-9 and 1e9
+                    account_tokens = generate_number(
+                        minimun, maximun, currency=token_name)
 
-                accounts.append((token_name, account_tokens))
-            random_token_list.append(random_token)
+                    accounts.append((token_name, account_tokens))
+                random_token_list.append(random_token)
 
         client_data = {
             client_id: accounts,
